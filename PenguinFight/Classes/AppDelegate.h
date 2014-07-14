7b77a48d46ebd7a9e191ea28703175d857185d3e
Vasani <vasanis5735@admins-iMac-3.local>
2014-07-14 13:54:12 -0500

    Initial Commit

---
+++ PenguinFight/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  PenguinFight
+//
+//  Created by Vasani, Sandeep on 7/14/14.
+//  Copyright ___Sandeep Vasani___ 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ PenguinFight/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PenguinFight
+//
+//  Created by Vasani, Sandeep on 7/14/14.
+//  Copyright ___Sandeep Vasani___ 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
